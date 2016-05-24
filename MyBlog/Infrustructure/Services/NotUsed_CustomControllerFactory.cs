16d158a530187ac5fdadb34464376e161aa603a1
AleksanderGalkin <agalkinov@mail.ru>
2016-05-24 18:44:39 +0700

    просто так

---
+++ MyBlog/Infrustructure/Services/NotUsed_CustomControllerFactory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.SessionState;
+
+namespace MyBlog.Infrastructure.Services
+{
+    public class CustomControllerFactory : IControllerFactory
+    {
+        private readonly DefaultControllerFactory _defaultControllerFactory;
+
+        public CustomControllerFactory()
+        {
+            _defaultControllerFactory = new DefaultControllerFactory();
+        }
+
+        public IController CreateController(RequestContext requestContext, string controllerName)
+        {
+
+            //var controller = PlugInFactory.GetObject(controllerName);
+
+            //if (controller == null)
+            var controller = _defaultControllerFactory.CreateController(requestContext, controllerName);
+                if (controller == null)
+                    throw new Exception("Controller not found!");
+                return controller;
+            
+        }
+
+        public SessionStateBehavior GetControllerSessionBehavior(RequestContext requestContext, string controllerName)
+        {
+            return SessionStateBehavior.Default;
+        }
+
+        public void ReleaseController(IController controller)
+        {
+            var disposableController = controller as IDisposable;
+
+            if (disposableController != null)
+            {
+                disposableController.Dispose();
+            }
+        }
+
+    }
+}
